83d0faeeb196731d26d6e60cd270ba82a5326623
Damarapelly-Sathish <168978053+Damarapelly-Sathish@users.noreply.github.com>
2024-07-29 19:02:20 +0530

    added add and get products

--- src/main/java/com/e_commerce/e_commerceWebsite/Service/DressProductService.java
+++ src/main/java/com/e_commerce/e_commerceWebsite/Service/DressProductService.java
@@ -1,12 +1,12 @@
 
 package com.e_commerce.e_commerceWebsite.Service;
-
 // import org.apache.commons.io.IOUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.e_commerce.e_commerceWebsite.Model.DressProduct;
+import com.e_commerce.e_commerceWebsite.Model.Product;
 import com.e_commerce.e_commerceWebsite.Repo.DressProductRepository;
 
 import java.io.BufferedReader;
@@ -14,6 +14,7 @@ import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.List;
 
 @Service
 public class DressProductService {
@@ -21,27 +22,44 @@ public class DressProductService {
     @Autowired
     private DressProductRepository dressProductRepository;
 
-    public void saveProductsFromFile(MultipartFile file, String imagesDir) throws Exception {
+    public List<DressProduct> saveProductsFromFile(MultipartFile file, String imagesDir) throws Exception {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(file.getInputStream()))) {
             String line;
             while ((line = br.readLine()) != null) {
                 String[] fields = line.split(",");
+                System.out.println("filed length"+fields.length);
+                if (fields.length < 4) {
+                    // Skip this line or handle the error appropriately
+                    System.out.println("Invalid line:");
+   
+                }
+
                 String name = fields[0];
                 String description = fields[1];
-                Double price = Double.parseDouble(fields[2]);
+                Double price = null;
+                try {
+                    price = Double.parseDouble(fields[2]);
+                } catch (NumberFormatException e) {
+                    // Skip this line or handle the error appropriately
+                    System.out.println("Invalid price: " + fields[2]);
+                   
+                }
                 String imageName = fields[3];
 
-                Path imagePath = Paths.get(imagesDir, imageName);
-                byte[] image = Files.readAllBytes(imagePath);
 
                 DressProduct product = new DressProduct();
                 product.setName(name);
                 product.setDescription(description);
                 product.setPrice(price);
-                product.setImage(image);
+                product.setImage(imageName);
 
                 dressProductRepository.save(product);
             }
         }
+		return null;
     }
+
+	public List<DressProduct> getProducts() {
+		return dressProductRepository.findAll();
+	}
 }
