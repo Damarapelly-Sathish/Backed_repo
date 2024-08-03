f70258c65c43d5c25375ebdf90709352b3345342
Damarapelly-Sathish <168978053+Damarapelly-Sathish@users.noreply.github.com>
2024-08-03 13:08:52 +0530

    Added columns and filter api

--- src/main/java/com/e_commerce/e_commerceWebsite/Repo/DressProductRepository.java
+++ src/main/java/com/e_commerce/e_commerceWebsite/Repo/DressProductRepository.java
@@ -1,7 +1,15 @@
 package com.e_commerce.e_commerceWebsite.Repo;
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.e_commerce.e_commerceWebsite.Model.DressProduct;
 
 public interface DressProductRepository extends JpaRepository<DressProduct, Long> {
+	@Query(value="SELECT * FROM dress_product  WHERE  name=:name",nativeQuery=true)
+	List<DressProduct> findByName(@Param("name") String name);
+	@Query(value="SELECT * FROM dress_product  WHERE  category=:category",nativeQuery=true)
+    List<DressProduct> findByCategory(@Param("category") String category);
 }
